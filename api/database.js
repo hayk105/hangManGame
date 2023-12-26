582d45e75ee05c33dafbb74d08ed332dfd51c448
Hayk Antonyan <hayka5367@gmail.com>
2023-12-26 19:26:50 +0400

    change data in database added gitignore file and added images animals

--- api/database.js
+++ api/database.js
@@ -1,5 +1,20 @@
 export default [
     {
         "name": "Animals1",
+        "image": "https://www.clipartmax.com/png/middle/329-3293428_cartoon-lion-clipart-lion-clipart-cute-borders-vectors-clipart-baby-jungle-animals.png",
+        "items": [
+            {
+                "name": "cat",
+            },
+            {
+                "name" : "frog",
+            },
+            {
+                "name" : "chicken",
+            },
+            {
+                "name" : "turtle",
+            }
+        ]
     }
 ];
